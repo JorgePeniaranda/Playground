dbed67ebeb758b5f6dc19a608f78a347053ae06f
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2026-04-04 09:54:07 -0300

    chore: update Angular and related dependencies to latest versions in package.json and package-lock.json for improved stability and performance

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,4 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import ignores from './configs/eslint/ignores.mjs';
 import { base, node, typescript } from './configs/eslint/index.mjs';
 
-export default defineConfig(
-  base,
-  node,
-  typescript,
-  globalIgnores(ignores),
-);
+export default defineConfig(base, node, typescript, globalIgnores(ignores));
